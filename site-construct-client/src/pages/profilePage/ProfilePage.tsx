93c718bf7062718da0dda74499126193f2b3af7e
polya <polyakrupina@gmail.com>
2025-04-25 14:39:54 +0500

    create notifications modal

--- site-construct-client/src/pages/profilePage/ProfilePage.tsx
+++ site-construct-client/src/pages/profilePage/ProfilePage.tsx
@@ -7,6 +7,7 @@ import ProfileMessages from '../../components/profileMessages/ProfileMessages';
 import ProfileOrders from '../../components/profileOrders/ProfileOrders';
 import ProfilePaymentMethods from '../../components/profilePaymentMethods/ProfilePaymentMethods';
 import ProfileReviews from '../../components/profileReviews/ProfileReviews';
+import Modal from '../../components/modal/Modal';
 
 const ProfilePage: React.FC = () => {
     const [selectedSection, setSelectedSection] = useState<'orders' | 'payment' | 'messages' | 'reviews' | 'profile'>('orders');
@@ -25,6 +26,8 @@ const ProfilePage: React.FC = () => {
                 return <ProfileOrders />;
         }
     };
+
+    const [isNotifyModalOpen, setIsNotifyModalOpen] = useState(false);
   
     return (
         <div className={`${selectedSection === 'messages' ? 'page-content__no-pad' : 'page-content'}`}>
@@ -42,12 +45,18 @@ const ProfilePage: React.FC = () => {
                                 <path fillRule="evenodd" clipRule="evenodd" d="M0.46967 0.46967C0.176777 0.762563 0.176777 1.23744 0.46967 1.53033L5.93934 7L0.46967 12.4697C0.176777 12.7626 0.176777 13.2374 0.46967 13.5303C0.762563 13.8232 1.23744 13.8232 1.53033 13.5303L7.53033 7.53033C7.82322 7.23744 7.82322 6.76256 7.53033 6.46967L1.53033 0.46967C1.23744 0.176777 0.762563 0.176777 0.46967 0.46967Z" fill="black"/>
                             </svg>
                         </div>
-                        <div className='profile-nav_notify'>
+                        <button
+                            className='profile-nav_notify'
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setIsNotifyModalOpen(true);
+                            }}
+                        >
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M7.21026 3.36156C8.47184 2.01587 10.1937 1.25 12.0001 1.25C13.8065 1.25 15.5283 2.01587 16.7898 3.36156C18.0499 4.70561 18.7501 6.51876 18.7501 8.4C18.7501 12.0306 19.4787 14.3109 20.1678 15.6585C20.5131 16.3338 20.8518 16.7814 21.0921 17.052C21.2124 17.1875 21.3086 17.2792 21.3694 17.3332C21.3998 17.3602 21.4214 17.3779 21.4328 17.3869L21.4415 17.3937C21.7026 17.5837 21.8129 17.9199 21.7145 18.2281C21.6153 18.539 21.3264 18.75 21.0001 18.75H3.00005C2.67373 18.75 2.38485 18.539 2.28559 18.2281C2.18718 17.9199 2.29755 17.5837 2.55863 17.3937L2.56735 17.3869C2.57869 17.3779 2.60028 17.3602 2.63069 17.3332C2.69148 17.2792 2.7877 17.1875 2.90804 17.052C3.14835 16.7814 3.48701 16.3338 3.8323 15.6585C4.52142 14.3109 5.25005 12.0306 5.25005 8.4C5.25005 6.51876 5.95021 4.70561 7.21026 3.36156ZM19.3612 17.25C19.189 16.9905 19.01 16.689 18.8323 16.3415C18.0214 14.7558 17.2501 12.2361 17.2501 8.4C17.2501 6.88647 16.6859 5.44389 15.6955 4.38747C14.7067 3.33269 13.3762 2.75 12.0001 2.75C10.6239 2.75 9.29342 3.33269 8.30457 4.38747C7.31417 5.44389 6.75005 6.88647 6.75005 8.4C6.75005 12.2361 5.97869 14.7558 5.16781 16.3415C4.99009 16.689 4.81108 16.9905 4.63891 17.25H19.3612ZM2.56447 17.3895C2.56446 17.3895 2.56446 17.3895 2.56446 17.3895L2.56393 17.3898L2.56357 17.3901L2.56347 17.3902C2.56362 17.3901 2.56378 17.3899 2.56393 17.3898C2.56411 17.3897 2.56429 17.3896 2.56447 17.3895Z" fill="black"/>
                                 <path fillRule="evenodd" clipRule="evenodd" d="M9.89381 20.3513C10.2521 20.1434 10.7111 20.2654 10.9189 20.6237C11.0288 20.8131 11.1865 20.9703 11.3763 21.0796C11.566 21.1889 11.7812 21.2465 12.0001 21.2465C12.2191 21.2465 12.4343 21.1889 12.624 21.0796C12.8138 20.9703 12.9715 20.8131 13.0814 20.6237C13.2892 20.2654 13.7482 20.1434 14.1065 20.3513C14.4648 20.5591 14.5867 21.018 14.3789 21.3763C14.1372 21.7931 13.7902 22.139 13.3727 22.3794C12.9552 22.6199 12.4819 22.7465 12.0001 22.7465C11.5184 22.7465 11.0451 22.6199 10.6276 22.3794C10.2101 22.139 9.86313 21.7931 9.62139 21.3763C9.41355 21.018 9.53552 20.5591 9.89381 20.3513Z" fill="black"/>
                             </svg>
-                        </div>
+                        </button>
                     </div>
                     <ul className='profile-nav_list'>
                         <li className='profile-nav_item' onClick={() => setSelectedSection('payment')}>
@@ -83,6 +92,74 @@ const ProfilePage: React.FC = () => {
                     {renderSection()}
                 </div>
             </div>
+            <Modal
+                isOpen={isNotifyModalOpen}
+                onClose={() => setIsNotifyModalOpen(false)}
+                className="profile_notify-modal"
+            >
+                <h2 className='text-h2'>Уведомления</h2>
+                <button className='notify-modal_exit' onClick={() => setIsNotifyModalOpen(false)}>
+                    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M0.227072 0.22703C0.519965 -0.0658633 0.994839 -0.0658636 1.28773 0.22703L6.00004 4.93934L10.7124 0.22703C11.0052 -0.0658633 11.4801 -0.0658637 11.773 0.22703C12.0659 0.519923 12.0659 0.994797 11.773 1.28769L7.0607 6L11.773 10.7123C12.0659 11.0052 12.0659 11.4801 11.773 11.773C11.4801 12.0659 11.0052 12.0659 10.7124 11.773L6.00004 7.06066L1.28773 11.773C0.994839 12.0659 0.519965 12.0659 0.227072 11.773C-0.0658213 11.4801 -0.0658213 11.0052 0.227072 10.7123L4.93938 6L0.227072 1.28769C-0.0658213 0.994797 -0.0658213 0.519923 0.227072 0.22703Z" fill="#02040F"/>
+                    </svg>
+                </button>
+                <ul className='notify-modal_list'>
+                    <li className='notify-modal_list-item'>
+                        <div className="notify-item_img"></div>
+                        <p className="notify-item_text text-n16">
+                            Скидки на категорию <span className="notify-item_text__accent">Электроника для авто</span>. Успевайте купить!
+                        </p>
+                        <p className="notify-item_date text-n14">
+                            2 часа назад
+                        </p>
+                    </li>
+                    <li className='notify-modal_list-item'>
+                        <div className="notify-item_img"></div>
+                        <p className="notify-item_text text-n16">
+                            Скидки на категорию <span className="notify-item_text__accent">Электроника для авто</span>. Успевайте купить!
+                        </p>
+                        <p className="notify-item_date text-n14">
+                            2 часа назад
+                        </p>
+                    </li>
+                    <li className='notify-modal_list-item'>
+                        <div className="notify-item_img"></div>
+                        <p className="notify-item_text text-n16">
+                            Скидки на категорию <span className="notify-item_text__accent">Электроника для авто</span>. Успевайте купить!
+                        </p>
+                        <p className="notify-item_date text-n14">
+                            2 часа назад
+                        </p>
+                    </li>
+                    <li className='notify-modal_list-item'>
+                        <div className="notify-item_img"></div>
+                        <p className="notify-item_text text-n16">
+                            Скидки на категорию <span className="notify-item_text__accent">Электроника для авто</span>. Успевайте купить!
+                        </p>
+                        <p className="notify-item_date text-n14">
+                            2 часа назад
+                        </p>
+                    </li>
+                    <li className='notify-modal_list-item'>
+                        <div className="notify-item_img"></div>
+                        <p className="notify-item_text text-n16">
+                            Скидки на категорию <span className="notify-item_text__accent">Электроника для авто</span>. Успевайте купить!
+                        </p>
+                        <p className="notify-item_date text-n14">
+                            2 часа назад
+                        </p>
+                    </li>
+                    <li className='notify-modal_list-item'>
+                        <div className="notify-item_img"></div>
+                        <p className="notify-item_text text-n16">
+                            Скидки на категорию <span className="notify-item_text__accent">Электроника для авто</span>. Успевайте купить!
+                        </p>
+                        <p className="notify-item_date text-n14">
+                            2 часа назад
+                        </p>
+                    </li>
+                </ul>
+            </Modal>
         </div>
     );
 };
